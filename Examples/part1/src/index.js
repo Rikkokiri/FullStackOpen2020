6d671a498455dd53236f399b1a3557422885dcdf
Ossian Rajala <ossian.rajala@gmail.com>
2020-03-20 00:12:58 +0200

    Following along with part 1 examples

---
+++ Examples/part1/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name='Maya' />
+      <Hello name='Carol' />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
