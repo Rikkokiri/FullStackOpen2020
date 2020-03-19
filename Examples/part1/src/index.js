684172c20f050c40765808b52c2e58062178c177
Ossian Rajala <ossian.rajala@gmail.com>
2020-03-20 01:51:37 +0200

    Reading part 1c

--- Examples/part1/src/index.js
+++ Examples/part1/src/index.js
@@ -1,20 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = (props) => {
+const Hello = ({ name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age
+
   return (
     <div>
-      <p>Hello {props.name}</p>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>So you were probably born {bornYear()}</p>
     </div>
   )
 }
 
 const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+
+  const handleClick = () => {
+    console.log('Clicked!')
+  }
+
+  const nimi = 'Pekka'
+  const ika = 10
+
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello name='Maya' />
-      <Hello name='Carol' />
+      <div>{counter}</div>
+      <button onClick={handleClick}>Plus</button>
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={nimi} age={ika} />
     </div>
   )
 }
