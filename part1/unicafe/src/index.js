b4df587f8b3dd255468a97032593f819015a5204
Ossian Rajala <ossian.rajala@gmail.com>
2020-03-20 04:13:32 +0200

    Completed functionality of unicafe app and gave a bit css flair

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,12 +1,6 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-// import './index.css'
-
-const Button = ({ handleClick, text }) => {
-  return (
-    <button onClick={handleClick}>{text}</button>
-  )
-}
+import './index.css'
 
 const Statistics = ({ good, neutral, bad }) => {
   const count = () => {
@@ -17,22 +11,43 @@ const Statistics = ({ good, neutral, bad }) => {
     return (good - bad) / count()
   }
 
+  const positive = () => {
+    return good / count() + ' %'
+  }
+
   if (count() > 0) {
     return (
-      <div>
-        <p>Good {good}</p>
-        <p>Neutral {neutral}</p>
-        <p>Bad {bad}</p>
-        <p>All {count()}</p>
-        <p>Average {average()}</p>
-        <p>Positive {good / count()} %</p>
-      </div>
+      <table>
+        <tbody>
+          <StatisticLine text='Good' value={good} />
+          <StatisticLine text='Neutral' value={neutral} />
+          <StatisticLine text='Bad' value={bad} />
+          <StatisticLine text='All' value={count()} />
+          <StatisticLine text='Average' value={average()} />
+          <StatisticLine text='Positive' value={positive()} />
+        </tbody>
+      </table>
     )
   } else {
     return (
       <div>No feedbacks given</div>
     )
   }
+}
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick} className='Button'>{text}</button>
+  )
 }
 
 const App = () => {
@@ -45,19 +60,22 @@ const App = () => {
   const giveBadFeedback = () => setBad(bad + 1)
 
   return (
-    <div>
-      <h1>Give us feedback!</h1>
+    <div className='AppContainer'>
+      <h1>Give us <br /> feedback...</h1>
       <Button
         handleClick={giveGoodFeedback}
-        text='good' />
+        text='Good' />
       <Button
         handleClick={giveNeutralFeedback}
-        text='neutral' />
+        text='Neutral' />
       <Button
         handleClick={giveBadFeedback}
-        text='bad' />
-      <h2>Statistics</h2>
-      <Statistics good={good} bad={bad} neutral={neutral} />
+        text='Bad' />
+
+      <div className='StatsContainer'>
+        <h2>Statistics</h2>
+        <Statistics good={good} bad={bad} neutral={neutral} />
+      </div>
     </div>
   )
 }
