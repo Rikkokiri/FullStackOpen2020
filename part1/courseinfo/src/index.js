53e86745d3be107cde8267467318df1458ead586
Ossian Rajala <ossian.rajala@gmail.com>
2020-03-20 05:27:06 +0200

    Ad id values to course parts to allow using map function

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -10,7 +10,7 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      {props.parts.map(part => <Part part={part} />)}
+      {props.parts.map(part => <Part key={part.id} part={part} />)}
     </div>
   )
 }
@@ -34,14 +34,17 @@ const App = () => {
     name: 'Half Stack application development',
     parts: [
       {
+        id: 1,
         name: 'Fundamentals of React',
         exercises: 10
       },
       {
+        id: 2,
         name: 'Using props to pass data',
         exercises: 7
       },
       {
+        id: 3,
         name: 'State of a component',
         exercises: 14
       }
