9b304c522f0628ada9cb0f626d27834144d3137e
Ossian Rajala <ossian.rajala@gmail.com>
2020-03-24 14:44:42 +0200

    Added filtering function to phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,16 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1231244' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filterInput, setFilterInput] = useState('')
+
+  const personsToShow = filterInput === '' ? persons : persons.filter(person => person.name.toLowerCase().includes(filterInput.toLowerCase()))
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
@@ -15,6 +21,11 @@ const App = () => {
   const handleNumberChange = (event) => {
     console.log(event.target.value)
     setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setFilterInput(event.target.value)
   }
 
   const addPerson = (event) => {
@@ -45,6 +56,13 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
+          <label htmlFor='filter'>Filter displayed numbers by name: </label>
+          <input id='filter'
+            value={filterInput}
+            onChange={handleFilterChange}
+          />
+        </div>
+        <div>
           <label htmlFor='name'>Name</label>
           <input id='name'
             value={newName}
@@ -64,11 +82,10 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
+        {personsToShow.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
       </ul>
     </div>
   )
-
 }
 
 export default App
