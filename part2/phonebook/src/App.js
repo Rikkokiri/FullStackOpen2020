ce4fe64f5412f3a27742b74a46aa0c8c19f9caad
Ossian Rajala <ossian.rajala@gmail.com>
2020-03-24 13:54:03 +0200

    It is possible to now add numbers to the phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,13 +2,19 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1231244' }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   const addPerson = (event) => {
@@ -17,10 +23,15 @@ const App = () => {
     // If the phone book already includes the name that user tries to add, prevent adding it.
     // (Case insensitive check)
     if (persons.some(p => p.name.toLowerCase() === newName.toLowerCase())) {
-      window.alert(`${newName} is already added to the phonebook.`)
+      window.alert(`Name '${newName}' is already in the phonebook.`)
+    } else if (persons.some(p => p.number === newNumber)) {
+      window.alert(`The number ${newNumber} is already in the phonebook.`)
+    } else if (newName === '' || newNumber === '') {
+      window.alert('Both fields need to be filled.')
     } else {
       const personObject = {
         name: newName,
+        number: newNumber,
         id: persons.length + 1
       }
 
@@ -34,10 +45,17 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name:
-          <input
+          <label htmlFor='name'>Name</label>
+          <input id='name'
             value={newName}
             onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <label htmlFor='number'>number:</label>
+          <input id='number'
+            value={newNumber}
+            onChange={handleNumberChange}
           />
         </div>
         <div>
@@ -46,7 +64,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => <li key={person.name}>{person.name}</li>)}
+        {persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
       </ul>
     </div>
   )
