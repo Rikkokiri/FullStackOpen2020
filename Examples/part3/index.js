86ace7790d5e9281554f78a51f19269a4f918ea4
Ossian Rajala <ossian.rajala@gmail.com>
2020-07-23 14:59:09 +0300

    Part 3 example follow along

--- Examples/part3/index.js
+++ Examples/part3/index.js
@@ -46,11 +46,27 @@ app.delete('api/notes/:id', (req, res) => {
   res.status(204).end()
 })
 
-app.post('/api/notes', (req, res) => {
-  const note = req.body
-
+const generateId = () => {
   const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
-  note.id = maxId + 1
+  return maxId + 1
+}
+
+app.post('/api/notes', (req, res) => {
+  const body = req.body
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'Content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId()
+  }
+
   notes = notes.concat(note)
 
   res.json(note)
